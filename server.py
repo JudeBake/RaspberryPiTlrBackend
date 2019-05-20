75b3b84d2b304b0721fdd01aab7e94b5b36f8881
jbacon <julien.bacon.1@gmail.com>
2019-05-20 13:42:50 -0400

    creating get handling function

--- server.py
+++ server.py
@@ -40,48 +40,52 @@ class StreamingOutput(object):
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/state':
-            json_str = json.dumps(timelapseRecorder.getState())
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/json')
-            self.end_headers()
-            self.wfile.write(json_str.encode(encoding='utf_8'))
-        elif self.path == '/video_feed':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
+        handleGet(self)
+
+# Requestion handling functions
+def handleGet(reqHandler):
+    if reqHandler.path == '/':
+        reqHandler.send_response(301)
+        reqHandler.send_header('Location', '/index.html')
+        reqHandler.end_headers()
+    elif reqHandler.path == '/index.html':
+        content = PAGE.encode('utf-8')
+        reqHandler.send_response(200)
+        reqHandler.send_header('Content-Type', 'text/html')
+        reqHandler.send_header('Content-Length', len(content))
+        reqHandler.end_headers()
+        reqHandler.wfile.write(content)
+    elif reqHandler.path == '/state':
+        json_str = json.dumps(timelapseRecorder.getState())
+        reqHandler.send_response(200)
+        reqHandler.send_header('Content-Type', 'application/json')
+        reqHandler.end_headers()
+        reqHandler.wfile.write(json_str.encode(encoding='utf_8'))
+    elif reqHandler.path == '/video_feed':
+        reqHandler.send_response(200)
+        reqHandler.send_header('Age', 0)
+        reqHandler.send_header('Cache-Control', 'no-cache, private')
+        reqHandler.send_header('Pragma', 'no-cache')
+        reqHandler.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+        reqHandler.end_headers()
+        try:
+            while True:
+                with output.condition:
+                    output.condition.wait()
+                    frame = output.frame
+                reqHandler.wfile.write(b'--FRAME\r\n')
+                reqHandler.send_header('Content-Type', 'image/jpeg')
+                reqHandler.send_header('Content-Length', len(frame))
+                reqHandler.end_headers()
+                reqHandler.wfile.write(frame)
+                reqHandler.wfile.write(b'\r\n')
+        except Exception as e:
+            logging.warning(
+                'Removed streaming client %s: %s',
+                reqHandler.client_address, str(e))
+    else:
+        reqHandler.send_error(404)
+        reqHandler.end_headers()
 
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
@@ -91,7 +95,7 @@ with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
     output = StreamingOutput()
     camera.start_recording(output, format='mjpeg')
     try:
-        timelapseRecorder = timelapseRecorder(camera)
+        timelapseRecorder = TimelapseRecorder(camera)
         address = ('', 5000)
         server = StreamingServer(address, StreamingHandler)
         server.serve_forever()
