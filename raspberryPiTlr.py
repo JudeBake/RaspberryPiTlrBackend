87c9ab2238be3569b2ef9d4dc61c51c53a763038
jbacon <julien.bacon.1@gmail.com>
2019-05-20 08:43:14 -0400

    test3

--- raspberryPiTlr.py
+++ raspberryPiTlr.py
@@ -20,10 +20,17 @@ def index():
     """Video streaming home page."""
     return render_template('index.html')
 
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(camera.get_frame(),
+    return Response(gen(camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 class RecorderState(Resource):
