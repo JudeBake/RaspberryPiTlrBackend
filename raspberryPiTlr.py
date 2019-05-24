6c1b74235054f1b6ed9619a61c7816924352cead
jbacon <julien.bacon.1@gmail.com>
2019-05-23 21:12:57 -0400

    disabling debug

--- raspberryPiTlr.py
+++ raspberryPiTlr.py
@@ -77,4 +77,4 @@ def onStartRecording(timelapseInfo):
                     timelapseInfo.timelapseName + ' started!'}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port =5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port =5000, debug=False, threaded=True)
